4e4680a0e5b15278e5451b71b941591a9577f92c
marcosfelipp <marcosabreu@inf.ufg.br>
2021-10-05 11:22:49 -0300

    Add registration

--- Flask/market/routes.py
+++ Flask/market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
-
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
+from market.forms import RegisterForm, LoginForm
+from market import db
 
 @app.route('/')
 def home_page():
@@ -12,3 +13,26 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(err_msg, category='danger')
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
